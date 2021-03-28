dc34720307c2371d8bf24f010d529c4addba8c3c
mika <mika.laihanen@gmail.com>
2021-03-28 14:16:52 +0300

    Add error boundary

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import pet from '@frontendmasters/pet';
 import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
 
 class Details extends React.Component {
     // props is data coming from the parent component
@@ -51,4 +52,16 @@ class Details extends React.Component {
         )
     }
 }
-export default Details;
+
+// Details uses the API so there's a chance for error.
+// Because of this, let's instead of exporting details, 
+// wrap it in a higher order component
+
+// export default Details;
+export default function DetailsWithErrorBoundary(props) {
+    return (
+        <ErrorBoundary>
+            <Details {...props} />
+        </ErrorBoundary>
+    )
+}
